455b4f94e1a19fe12b17f6d966cc914d0176a0d3
rensralph <rensralph.n@gmail.com>
2020-01-10 02:16:11 +0800

    new

--- lib/core/services/database.dart
+++ lib/core/services/database.dart
@@ -1,41 +1,62 @@
+import 'dart:io';
+
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:base/note/data/models.dart';
+import 'package:path_provider/path_provider.dart';
 
 class NotesDatabaseService {
+  static final _databaseName = "memo.db";
+  static final _databaseVersion = 1;
+
+  static final table = 'memo';
+
+  static final columnId = '_id';
+  static final columnTitle = 'title';
+  static final columnContent = 'content';
+  static final columnIsImportant = 'isImportant';
+  static final columnDate = 'date';
+
   String path;
 
-  NotesDatabaseService._();
+  NotesDatabaseService._privateConstructor();
+  static final NotesDatabaseService db =
+      NotesDatabaseService._privateConstructor();
 
-  static final NotesDatabaseService db = NotesDatabaseService._();
-
-  Database _database;
+  static Database _database;
 
   Future<Database> get database async {
-    if (_database != null) return _database;
+    if (_database != null) {
+      return _database;
+    }
     // if _database is null we instantiate it
     _database = await init();
     return _database;
   }
 
   init() async {
-    String path = await getDatabasesPath();
-    path = join(path, 'notes.db');
-    print("Entered path $path");
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    String path = join(documentsDirectory.path, _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
 
-    return await openDatabase(path, version: 1,
-        onCreate: (Database db, int version) async {
-      await db.execute(
-          'CREATE TABLE Notes ( _id INTEGER PRIMARY KEY, title TEXT, content TEXT, isImportant INTEGER, date TEXT;');
-      print('New table created at $path');
-    });
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE $table ( 
+      $columnId INTEGER PRIMARY KEY,
+      $columnTitle TEXT,
+      $columnContent TEXT,
+      $columnIsImportant INTEGER,
+      $columnDate TEXT
+      )
+      ''');
   }
 
   Future<List<NotesModel>> getNotesFromDB() async {
     final db = await database;
-    List<NotesModel> notesList = [];  
-    List<Map> maps = await db.query('Notes',
-        columns: ['_id', 'title', 'content', 'isImportant', 'date']);
+    List<NotesModel> notesList = [];
+    List<Map> maps = await db.query(table);
     if (maps.length > 0) {
       maps.forEach((map) {
         notesList.add(NotesModel.fromMap(map));
@@ -46,23 +67,21 @@ class NotesDatabaseService {
 
   updateNoteInDB(NotesModel updatedNote) async {
     final db = await database;
-    await db.update('Notes', updatedNote.toMap(),
-        where: '_id = ?', whereArgs: [updatedNote.id]);
-    print('Note updated: ${updatedNote.title} ${updatedNote.content}');
+    await db.update(table, updatedNote.toMap(),
+        where: '$columnId = ?', whereArgs: [updatedNote.id]);
   }
 
   deleteNoteInDB(NotesModel noteToDelete) async {
     final db = await database;
-    await db.delete('Notes', where: '_id = ?', whereArgs: [noteToDelete.id]);
+    await db.delete('Notes', where: '$columnId = ?', whereArgs: [noteToDelete.id]);
     print('Note deleted');
   }
 
   Future<NotesModel> addNoteInDB(NotesModel newNote) async {
     final db = await database;
-    if (newNote.title.trim().isEmpty) 
-    newNote.title = 'Untitled Note';
+    if (newNote.title.trim().isEmpty) newNote.title = 'Untitled Note';
     int id = await db.transaction((transaction) => transaction.rawInsert(
-          'INSERT into Notes (title, content, isImportant, date ) VALUES ("${newNote.title}", "${newNote.content}", "${newNote.isImportant == true ? 1 : 0}", "${newNote.date.toIso8601String()}");'));
+        'INSERT into $table ($columnTitle, $columnContent, $columnIsImportant, $columnDate ) VALUES ("${newNote.title}", "${newNote.content}", "${newNote.isImportant == true ? 1 : 0}", "${newNote.date.toIso8601String()}");'));
     newNote.id = id;
     print('Note added: ${newNote.title} ${newNote.content}');
     return newNote;
